c997ff8e3302fed29f937905c8c0a36ae32a3e80
Angshuman <angshumanroy200@gmail.com>
2025-08-01 19:18:04 +0530

    done

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) throw err;
-  console.log('MySQL connected');
+  console.log("MySQL connected");
 });
 
 module.exports = db;
